88046f893e476e5c2771c58438c1265848300457
A-Hacker-06 <101251930+A-Hacker-06@users.noreply.github.com>
2022-06-12 11:30:39 -0700

    Add files via upload

---
+++ name.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "What is your name?"
+
+read name 
+
+echo "Assalam-O-Alaikum......!! $name!!" 
+sleep 1
+echo "You're looking good today $name!!"
+sleep 1
+echo "You're to much good janu.......!! $name!!"
